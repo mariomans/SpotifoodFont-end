9ea22f475daa38348fa5d158820778a5b630e43a
Unknown <frong_98_@hotmail.com>
2019-10-06 19:35:07 +0700

    Test pin button on user and post

--- src/user/ProfilesTab.js
+++ src/user/ProfilesTab.js
@@ -4,29 +4,45 @@ import DefaultProfile from "../images/avatar.jpg";
 
 class ProfileTabs extends Component {
     render() {
-        const { posts } = this.props;
+        const { posts, following, followers } = this.props;
         return (
-            <div className="row">
-                <div className="col-md-4">
-                    <h3 className="text-primary">Posts</h3>
-                    <hr />
-                    {posts.map((post, i) => (
-                        <div key={i}>
-                            <div>
-                                <Link to={`/post/${post._id}`}>
-                                    <div>
-                                        <p className="lead">
-                                            {post.title}
-                                        </p>
-                                    </div>
-                                </Link>
-                             </div>
-                        </div>
-                                ))}
+            // <div className="row">
+            //     <div className="col-md-4">
+            //         <h3 className="text-primary">Posts</h3>
+            //         <hr />
+            //         {posts.map((post, i) => (
+            //             <div key={i}>
+            //                 <div>
+            //                     <Link to={`/post/${post._id}`}>
+            //                         <div>
+            //                             <p className="lead">
+            //                                 {post.title}
+            //                             </p>
+            //                         </div>
+            //                     </Link>
+            //                  </div>
+            //             </div>
+            //                     ))}
+            //     </div>
+            // </div>
+
+            <div>
+                <div>
+                    following
+                    {JSON.stringify(following)}
+                </div>
+                <div>
+                    followers
+                    {JSON.stringify(followers)}
+                </div>
+                <div>
+                    posts
+                    {JSON.stringify(posts)}
                 </div>
             </div>
-                            )
-                        }
-                    }
-                    
+
+        )
+    }
+}
+
 export default ProfileTabs;
