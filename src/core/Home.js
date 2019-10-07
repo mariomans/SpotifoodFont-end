0dc5cb74ba1d7d6358767cce6c3108c075b9fe3b
Unknown <frong_98_@hotmail.com>
2019-10-07 13:30:39 +0700

    Edit some user interface on homepage

--- src/core/Home.js
+++ src/core/Home.js
@@ -4,6 +4,10 @@ import Picture from '../images/spotifood.jpg'
 
 const Home = () => (
     <div>
+        <div className="jumbotron">
+            <h2>Homepage</h2>
+            <p className="lead">Welcome to Spotifood</p>
+        </div>
         <div className="container">
             <Posts />
         </div>
