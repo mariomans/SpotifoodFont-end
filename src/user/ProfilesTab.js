e8531bee1ced377db75356c4beb888597b424109
Unknown <frong_98_@hotmail.com>
2019-10-07 19:36:23 +0700

    Bug fix profile tap show posts
    Show post only user post not all post and add some following and follower

--- src/user/ProfilesTab.js
+++ src/user/ProfilesTab.js
@@ -1,47 +1,103 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import DefaultProfile from "../images/avatar.jpg";
 
 class ProfileTabs extends Component {
     render() {
-        const { posts, following, followers } = this.props;
+        const { following, followers, posts } = this.props;
         return (
-            // <div className="row">
-            //     <div className="col-md-4">
-            //         <h3 className="text-primary">Posts</h3>
-            //         <hr />
-            //         {posts.map((post, i) => (
-            //             <div key={i}>
-            //                 <div>
-            //                     <Link to={`/post/${post._id}`}>
-            //                         <div>
-            //                             <p className="lead">
-            //                                 {post.title}
-            //                             </p>
-            //                         </div>
-            //                     </Link>
-            //                  </div>
-            //             </div>
-            //                     ))}
-            //     </div>
-            // </div>
-
             <div>
-                <div>
-                    following
-                    {JSON.stringify(following)}
-                </div>
-                <div>
-                    followers
-                    {JSON.stringify(followers)}
-                </div>
-                <div>
-                    posts
-                    {JSON.stringify(posts)}
+                <div className="row">
+                    <div className="col-md-4">
+                        <h3 className="text-primary">
+                            {followers.length} Followers
+                        </h3>
+                        <hr />
+                        {followers.map((person, i) => (
+                            <div key={i}>
+                                <div>
+                                    <Link to={`/user/${person._id}`}>
+                                        <img
+                                            style={{
+                                                borderRadius: "50%",
+                                                border: "1px solid black"
+                                            }}
+                                            className="float-left mr-2"
+                                            height="30px"
+                                            width="30px"
+                                            onError={i =>
+                                                (i.target.src = `${DefaultProfile}`)
+                                            }
+                                            src={`${
+                                                process.env.REACT_APP_API_URL
+                                            }/user/photo/${person._id}`}
+                                            alt={person.name}
+                                        />
+                                        <div>
+                                            <p className="lead">
+                                                {person.name}
+                                            </p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="col-md-4">
+                        <h3 className="text-primary">
+                            {following.length} Following
+                        </h3>
+                        <hr />
+                        {following.map((person, i) => (
+                            <div key={i}>
+                                <div>
+                                    <Link to={`/user/${person._id}`}>
+                                        <img
+                                            style={{
+                                                borderRadius: "50%",
+                                                border: "1px solid black"
+                                            }}
+                                            className="float-left mr-2"
+                                            height="30px"
+                                            width="30px"
+                                            onError={i =>
+                                                (i.target.src = `${DefaultProfile}`)
+                                            }
+                                            src={`${
+                                                process.env.REACT_APP_API_URL
+                                            }/user/photo/${person._id}`}
+                                            alt={person.name}
+                                        />
+                                        <div>
+                                            <p className="lead">
+                                                {person.name}
+                                            </p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="col-md-4">
+                        <h3 className="text-primary">{posts.length} Posts</h3>
+                        <hr />
+                        {posts.map((post, i) => (
+                            <div key={i}>
+                                <div>
+                                    <Link to={`/post/${post._id}`}>
+                                        <div>
+                                            <p className="lead">{post.title}</p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-
-        )
+        );
     }
 }
 
