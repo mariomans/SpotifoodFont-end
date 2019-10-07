1ff996d29ed412c48f9656c7f31bfd2517faabd5
Unknown <frong_98_@hotmail.com>
2019-10-07 13:30:20 +0700

    Create personalization page

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -12,6 +12,7 @@ import EditPost from './post/EditPost';
 import SinglePost from './post/SinglePost'
 import PrivateRoute from './auth/PrivateRoute'
 import Admin from './admin/Admin'
+// import Personalization from './user/Personalization'
 
 const MainRouter = () => (
     <div>
@@ -27,6 +28,7 @@ const MainRouter = () => (
             <PrivateRoute exact path="/user/edit/:userId" component={EditProfile}></PrivateRoute>
             <PrivateRoute exact path="/user/:userId" component={Profile}></PrivateRoute>
             <PrivateRoute exact path="/admin" component={Admin} />
+            {/* <PrivateRoute exact path="/personalization" component={Personalization} /> */}
         </Switch>
 
     </div>
